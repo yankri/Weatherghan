cb5bc116f3b5ad85e0cba061c4182f99e93faa23
Krista Scholdberg <krista.scholdberg@gmail.com>
2016-04-28 18:30:17 -0400

    file dump from MVCWeatherghan

---
+++ MVCWeatherghan/MVCWeatherghan/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCWeatherghan.Startup))]
+namespace MVCWeatherghan
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
