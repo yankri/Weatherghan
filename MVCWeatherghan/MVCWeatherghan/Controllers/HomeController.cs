fd606f000c57cd76100f7977735f745d43711e16
yankri <krista.scholdberg@gmail.com>
2016-09-21 09:32:10 -0400

    Update 9-21

--- MVCWeatherghan/MVCWeatherghan/Controllers/HomeController.cs
+++ MVCWeatherghan/MVCWeatherghan/Controllers/HomeController.cs
@@ -7,7 +7,10 @@ using System.Web;
 using System.Web.Mvc;
 using System.Text;
 using Rotativa;
+using System.Net.Mail;
 using MVCWeatherghan.Models;
+using System.Net;
+using System.Threading;
 
 namespace MVCWeatherghan.Controllers
 {
@@ -32,7 +35,7 @@ namespace MVCWeatherghan.Controllers
             return View(wd.PatternRows);
         }
 
-        public ActionResult DisplayPatternPDFView (string zipcode, string year)//parameters are query strings from the index view input boxes
+        public ActionResult DisplayPatternPDFView(string zipcode, string year)//parameters are query strings from the index view input boxes
         {
             Weatherdata wd = new Weatherdata();
             wd.ZipCode = zipcode;
@@ -46,7 +49,7 @@ namespace MVCWeatherghan.Controllers
             return View();
         }
 
-        public ActionResult Resources ()
+        public ActionResult Resources()
         {
             return View();
         }
@@ -56,22 +59,114 @@ namespace MVCWeatherghan.Controllers
 
             return View();
         }
-
-        public ActionResult Contact() //not used yet
+        [HttpGet]
+        public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
+            return View(new ContactForm());
+        }
 
+        [HttpPost]
+        public ActionResult Contact(ContactForm e) //not used yet
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    var toAddress = "weatherghan@gmail.com";
+                    var fromAddress = e.Email.ToString();
+                    var subject = String.Format("Test enquiry from {0} {1}", e.FirstName, e.LastName);
+                    var message = new StringBuilder();
+                    message.Append(String.Format("Name {0} {1} \n", e.FirstName, e.LastName));
+                    message.Append("Email: " + e.Email + "\n");
+                    message.Append("Message: " + e.Message + "\n\n");
+                    message.Append(e.Message);
+
+                    //start email Thread
+                    var tEmail = new Thread(() =>
+                   SendEmail(toAddress, fromAddress, subject, message.ToString()));
+                    tEmail.Start();
+                    return View("Success");
+                }
+                catch (Exception)
+                {
+                    return View("Error");
+                }
+        }
             return View();
-        }
-           
-        public ActionResult PatternToPDF(string zip, string viewyear)
-        {
-            return new ActionAsPdf("DisplayPatternPDFView", new { zipcode = zip, year = viewyear }) { FileName = "MyWeatherghanPattern.pdf" };
-        }
-
-        public ActionResult Gallery() //gallery page
-        {
-            return View();
-        }
     }
+        public void SendEmail(string toAddress, string fromAddress, string subject, string message)
+        {
+            try
+            {
+                using (var mail = new MailMessage())
+                {
+                    const string email = "weatherghan@gmail.com";
+                    const string password = "rileybung11";
+
+                    var loginInfo = new NetworkCredential(email, password);
+
+                    mail.From = new MailAddress(fromAddress);
+                    mail.To.Add(new MailAddress(toAddress));
+                    mail.Subject = subject;
+                    mail.Body = message;
+                    mail.IsBodyHtml = true;
+
+                    try
+                    {
+                        using (var smtpClient = new SmtpClient("smtp.gmail.com", 587))
+                        {
+                            smtpClient.EnableSsl = true;
+                            smtpClient.UseDefaultCredentials = false;
+                            smtpClient.Credentials = loginInfo;
+                            smtpClient.SendMailAsync(mail);
+                        }
+                    }
+                    finally
+                    {
+                        //dispose the client
+                        mail.Dispose();
+                    }
+                }
+            }
+            catch (SmtpFailedRecipientsException ex)
+            {
+                foreach (SmtpFailedRecipientException t in ex.InnerExceptions)
+                {
+                    var status = t.StatusCode;
+                    if (status == SmtpStatusCode.MailboxBusy ||
+                        status == SmtpStatusCode.MailboxUnavailable)
+                    {
+                        Response.Write("Delivery failed - retrying in 5 seconds.");
+                        System.Threading.Thread.Sleep(5000);
+                        //resend
+                        //smtpClient.Send(message);
+                    }
+                    else
+                    {
+                        Response.Write(String.Format("Failed to deliver message to {0}", t.FailedRecipient));
+                    }
+                }
+            }
+            catch (SmtpException Se)
+            {
+                // handle exception here
+                Response.Write(Se.ToString());
+            }
+
+            catch (Exception ex)
+            {
+                Response.Write(ex.ToString());
+            }
+
+        }
+        public ActionResult PatternToPDF(string zip, string viewyear)
+    {
+        return new ActionAsPdf("DisplayPatternPDFView", new { zipcode = zip, year = viewyear }) { FileName = "MyWeatherghanPattern.pdf" };
+    }
+
+    public ActionResult Gallery() //gallery page
+    {
+        return View();
+    }
+}
 }
---
+++ MVCWeatherghan/MVCWeatherghan/Models/ContactForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MVCWeatherghan.Models
+{
+    public class ContactForm
+    {
+        [Required(ErrorMessage="First Name is required.")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required.")]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage="A valid email is required.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage="A message is required.")]
+        public string Message { get; set; }
+    }
+}
